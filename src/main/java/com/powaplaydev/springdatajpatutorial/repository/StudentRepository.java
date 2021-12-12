dc2c1803dccd49ccb0908281653b0d50433255ee
powaplay <powaplaydev@gmail.com>
2021-12-12 10:52:38 -0500

    Defined methods for fetching data using JPQL and Native queries

--- src/main/java/com/powaplaydev/springdatajpatutorial/repository/StudentRepository.java
+++ src/main/java/com/powaplaydev/springdatajpatutorial/repository/StudentRepository.java
@@ -2,8 +2,55 @@ package com.powaplaydev.springdatajpatutorial.repository;
 
 import com.powaplaydev.springdatajpatutorial.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * QUERY CREATION DOCS: https://docs.spring.io/spring-data/jpa/docs/2.5.1/reference/html/#jpa.query-methods.query-creation
+ */
+
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    // find student by first name
+    List<Student> findByFirstName(String firstName);
+
+    // find student by first name that contains a certain string
+    List<Student> findByFirstNameContaining(String name);
+
+    List<Student> findByLastNameNotNull();
+
+    List<Student> findByGuardianName(String name);
+
+    Student findByFirstNameAndLastName(String firstName, String lastName);
+
+    // JPQL
+    @Query("select s from Student s where s.emailId = ?1")
+    Student getStudentByEmailAddress(String emailId);
+
+    // NOTE: we get s.firstName from instance variables of Student class (and NOT the DB column names)
+    @Query("select s.firstName from Student s where s.emailId = ?1")
+    String getStudentFirstNameByEmailAddress(String emailId);
+
+
+    // Native SQL Query (good when you have complex queries)
+    @Query(
+            value = "SELECT * FROM tbl_student s WHERE s.email_address = ?1",
+            nativeQuery = true
+    )
+    Student getStudentByEmailAddressNative(String emailId);
+
+
+    // Native Named Param
+    @Query(
+            value = "SELECT * FROM tbl_student s WHERE s.email_address = :emailId",
+            nativeQuery = true
+    )
+    Student getStudentByEmailAddressNativeNamedParam(@Param("emailId") String emailId);
+
+
 }
