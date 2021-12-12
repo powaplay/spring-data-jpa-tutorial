318339802db11fa928ee5168151bb4d0eda37956
powaplay <powaplaydev@gmail.com>
2021-12-12 08:41:36 -0500

    Created Student entity and repository

---
+++ src/main/java/com/powaplaydev/springdatajpatutorial/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.powaplaydev.springdatajpatutorial.repository;
+
+import com.powaplaydev.springdatajpatutorial.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
