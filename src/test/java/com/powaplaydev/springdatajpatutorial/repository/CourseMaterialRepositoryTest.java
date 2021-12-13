41598161563698b2064f43e0f0c85b968d3f78f6
powaplay <powaplaydev@gmail.com>
2021-12-13 10:27:40 -0500

    Created CourseMaterialRepository and test for saving course material

---
+++ src/test/java/com/powaplaydev/springdatajpatutorial/repository/CourseMaterialRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.powaplaydev.springdatajpatutorial.repository;
+
+import com.powaplaydev.springdatajpatutorial.entity.Course;
+import com.powaplaydev.springdatajpatutorial.entity.CourseMaterial;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseMaterialRepositoryTest {
+    @Autowired
+    private CourseMaterialRepository repository;
+
+    @Test
+    public void saveCourseMaterial() {
+        Course course = Course.builder()
+                .tile("DSA")
+                .credit(6)
+                .build();
+
+        CourseMaterial courseMaterial = CourseMaterial.builder()
+                .url("www.google.com")
+                .course(course)
+                .build();
+
+        repository.save(courseMaterial);
+    }
+}
